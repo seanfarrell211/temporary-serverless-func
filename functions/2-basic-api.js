265a42afad61442f66df1c341406cc7768b4c98c
sean <andhikasean21@gmail.com>
2021-03-08 14:34:14 +0700

    cors error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,9 +2,9 @@ const items = require('../assets/data')
 
 exports.handler = async (event,context) => {
     return {
-        // headers: {
-        //     'Access-Control-Allow-Origin': '*',
-        // }, // to let another file not in main file also working for its API
+         headers: {
+            'Access-Control-Allow-Origin': '*',
+         }, // to let another file not in main file also working for its API / cors error
     statusCode: 200,
     body:JSON.stringify(items),
     }
