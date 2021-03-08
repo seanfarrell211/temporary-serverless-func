f808a29904b27e34adec3fe4de2af55f29003ede
sean <andhikasean21@gmail.com>
2021-03-08 14:48:31 +0700

    try2

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,7 +3,7 @@ const items = require('../assets/data')
 exports.handler = async (event,context,cb) => {
     return {
          headers: {
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
          }, // to let another file not in main file also working for its API / cors error
     statusCode: 200,
     body:JSON.stringify(items),
