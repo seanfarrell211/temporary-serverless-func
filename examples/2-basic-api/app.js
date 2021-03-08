472f9690337f37fb0fcb8b2fd57c761b94a2cdb3
sean <andhikasean21@gmail.com>
2021-03-08 14:49:59 +0700

    try 3

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -3,7 +3,7 @@ const result = document.querySelector('.result')
 
 const fetchData = async () => {
 try {
-    const {data} = await axios.get('https://temp-serverless-sans.netlify.app/api/2-basic-api/')
+    const {data} = await axios.get('/api/2-basic-api/')
     const products = data.map((product) => {
     const {image:{url},name,price} = product
     return `<article class ="product">
