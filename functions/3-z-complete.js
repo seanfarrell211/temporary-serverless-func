0a425dea0d08b10ad8c8c0857c6198b024bf1b85
sean farrell <andhikasean21@gmail.com>
2021-04-29 12:33:02 +0700

    airtable update

--- functions/3-z-complete.js
+++ functions/3-z-complete.js
@@ -3,7 +3,7 @@ const Airtable = require('airtable-node');
  
 const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
   .base('appGsVXh5XoDKuAQc')
-  .table('SansFurniture')
+  .table('products')
 
 exports.handler = async (event, context, cb) => {
     const { id } = event.queryStringParameters
