959bfd660e2e8b8918592a852bc8ebf9a0a1e8ed
sean farrell <andhikasean21@gmail.com>
2021-04-29 17:17:50 +0700

    push

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -2,7 +2,6 @@ const result = document.querySelector('.result')
 
 const fetchProducts = async () => {
     try{
-    //    const {data} = await axios.get('/api/3-airtable');
     const {data} = await axios.get('/api/3-z-complete');
         const products = data
         .map((product) => {
