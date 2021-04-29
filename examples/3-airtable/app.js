79e5ff97a0db96dbb17fe8900acbe44caa70e067
sean farrell <andhikasean21@gmail.com>
2021-04-29 20:33:53 +0700

    push

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -5,9 +5,8 @@ const fetchProducts = async () => {
     const {data} = await axios.get('/api/3-z-complete');
         const products = data
         .map((product) => {
-            const{name,description,price,featured,colors,company,stock,stars,reviews,category,shipping,id,url,image} = product
+            const{name,description,price,featured,colors,company,stock,stars,reviews,category,shipping,id,image} = product
             return `<a href="product.html?id=${id}" class="product">
-            <img src="${url}" alt ="${name}"/>
             <div class ="info">
             <h5>${name}</h5>
             <h5 class="price">$${price}</h5>
