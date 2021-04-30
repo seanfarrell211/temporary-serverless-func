9905c96feb896630d746c5343a9290f3a4e1ab45
sean farrell <andhikasean21@gmail.com>
2021-05-01 00:23:55 +0700

    gd

--- functions/3-z-complete.js
+++ functions/3-z-complete.js
@@ -10,19 +10,26 @@ exports.handler = async (event, context, cb) => {
     const { id } = event.queryStringParameters
     if(id){
         try {
-            const product = await airtable.retrieve(id)
-            if(product.error){
-                return {
-                    statusCode: 404,
-                    body:`No product with id: ${id}`,
-                }
-            }
+            // const product = await airtable.retrieve(id)
+            // if(product.error){
+            //     return {
+            //         statusCode: 404,
+            //         body:`No product with id: ${id}`,
+            //     }
+            // }
+            const {records} = await airtable.list()
+            const products = records.map((product) => {
+                const{id} = product
+                const {name,images,price,description,colors,company,stock,stars,reviews,category,shipping} = product.fields
+                const image = images[0].url
+                return {id,name,image,price,colors,company,stock,stars,reviews,category,shipping,description}
+            })
             return {
                 headers: {
                     'Access-Control-Allow-Origin':'*',
                 },
                 statusCode:200,
-                body: JSON.stringify(product),
+                body: JSON.stringify(products),
             }
         }catch (error){
             return {
