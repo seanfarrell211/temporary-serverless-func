3e117c76c0ccf3958ae2e4728c35dc38676ea38d
sean farrell <andhikasean21@gmail.com>
2021-05-01 10:46:35 +0700

    test

--- functions/3-z-complete.js
+++ functions/3-z-complete.js
@@ -8,28 +8,27 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
 exports.handler = async (event, context, cb) => {
     console.log(event)
     const { id } = event.queryStringParameters
+    console.log(id)
     if(id){
         try {
-            // const product = await airtable.retrieve(id)
-            // if(product.error){
-            //     return {
-            //         statusCode: 404,
-            //         body:`No product with id: ${id}`,
-            //     }
-            // }
-            const {records} = await airtable.retrieve(id)
-            const products = records.map((product) => {
-                const{id} = product
-                const {name,images,price,description,colors,company,stock,stars,reviews,category,shipping} = product.fields
-                const image = images[0].url
-                return {id,name,image,price,colors,company,stock,stars,reviews,category,shipping,description}
-            })
+            const product = await airtable.retrieve(id)
+            if(product.error){
+                return {
+                    statusCode: 404,
+                    body:`No product with id: ${id}`,
+                }
+            }
+            
+            const{id} = product
+            const {name,images,price,description,colors,company,stock,stars,reviews,category,shipping} = product.fields
+            const image = images[0].url
             return {
                 headers: {
                     'Access-Control-Allow-Origin':'*',
                 },
                 statusCode:200,
-                body: JSON.stringify(products),
+                body: JSON.stringify({id,name,image,price,colors,company,stock,stars,reviews,category,shipping,description}),
+            
             }
         }catch (error){
             return {
@@ -40,19 +39,20 @@ exports.handler = async (event, context, cb) => {
     }
     try {
         const {records} = await airtable.list()
+        //console.log(records)
         const products = records.map((product) => {
             const{id} = product
             const {name,images,price,description,colors,company,stock,stars,reviews,category,shipping} = product.fields
             const image = images[0].url
             return {id,name,image,price,colors,company,stock,stars,reviews,category,shipping,description}
         })
-        console.log(products)
         return {
             headers: {
                 'Access-Control-Allow-Origin':'*',
             },
             statusCode: 200,
             body: JSON.stringify(products),
+           
         }
     }catch(error){
         return {
