a8ab256bf0c34cc3b16540fb8ffc136e34c73187
sean farrell <andhikasean21@gmail.com>
2021-05-01 11:08:31 +0700

    fix amin

--- functions/3-z-complete.js
+++ functions/3-z-complete.js
@@ -37,28 +37,29 @@ exports.handler = async (event, context, cb) => {
                 body: error,
             }
         }
-    }
-    try {
-        const {records} = await airtable.list()
-        //console.log(records)
-        const products = records.map((product) => {
-            const{id} = product
-            const {name,images,price,description,colors,company,stock,stars,reviews,category,shipping} = product.fields
-            const image = images[0].url
-            return {id,name,images,price,colors,company,stock,stars,reviews,category,shipping,description}
-        })
-        return {
-            headers: {
-                'Access-Control-Allow-Origin':'*',
-            },
-            statusCode: 200,
-            body: JSON.stringify(products),
-           
-        }
-    }catch(error){
-        return {
-            statusCode: 500,
-            body: error,
+    }else{
+        try {
+            const {records} = await airtable.list()
+            //console.log(records)
+            const products = records.map((product) => {
+                const{id} = product
+                const {name,images,price,description,colors,company,stock,stars,reviews,category,shipping} = product.fields
+                const image = images[0].url
+                return {id,name,images,price,colors,company,stock,stars,reviews,category,shipping,description}
+            })
+            return {
+                headers: {
+                    'Access-Control-Allow-Origin':'*',
+                },
+                statusCode: 200,
+                body: JSON.stringify(products),
+               
+            }
+        }catch(error){
+            return {
+                statusCode: 500,
+                body: error,
+            }
         }
     }
 }
